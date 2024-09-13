59a7fe11f30a781605ecec45aab800b3e0af0a2d
Fran Martin <fran@earthpulse.ai>
2024-09-13 18:41:23 +0200

    Refactor

--- scripts/renewable-energy/main.py
+++ scripts/renewable-energy/main.py
@@ -10,10 +10,34 @@ from spai.storage import Storage
 from spai.config import SPAIVars
 import geopandas as gpd
 
-from utils import create_buffer
-
 storage = Storage()["data"]
 vars = SPAIVars()
+
+
+def create_buffer(gdf: gpd.GeoDataFrame, buffer_size: int) -> gpd.GeoDataFrame:
+    """
+    Create a buffer around a GeoDataFrame.
+
+    Parameters
+    ----------
+    gdf : GeoDataFrame
+        The GeoDataFrame to buffer.
+    buffer_size : int
+        The buffer size in meters.
+
+    Returns
+    -------
+    GeoDataFrame
+        The GeoDataFrame with the buffer.
+    """
+    if not gdf.crs:
+        gdf = gdf.set_crs("EPSG:4326")
+    gdf_3857 = gdf.to_crs("EPSG:3857")
+    # Create a new geodataframe with the buffer
+    gdf_3857["geometry"] = gdf_3857.geometry.buffer(buffer_size)
+    gdf = gdf_3857.to_crs("EPSG:4326")
+
+    return gdf
 
 
 if __name__ == "__main__":
